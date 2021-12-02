c377cb035806a4ae810853b56c52bb9c58cf9d56
Andrew Siemen <andrew@andrewsiemen.com>
2021-12-01 20:58:35 -0500

    error in processing input, fixed off-by-one

--- AOC2021/day1.py
+++ AOC2021/day1.py
@@ -1,15 +1,14 @@
 #!/usr/bin/env python3
-import re
 
 input_file = 'inputs/day1'
 
 def process(given):
     with open(given) as f:
         infile = f.readlines()
-    infile = [x.strip() for x in infile]
+    infile = [int(x.strip()) for x in infile]
     return infile
 
-def p1(given): ### TODO: chasedown the stupid off-by-one error
+def p1(given): 
     ans = 0
     for i in range(1, len(given)):
         if given[i] > given[i-1]:
@@ -19,8 +18,8 @@ def p1(given): ### TODO: chasedown the stupid off-by-one error
 def p2(given):
     s1, s2, ans = 0, 0, 0
     for i in range(3, len(given)):
-        s1 = int(given[i-3]) + int(given[i-2]) + int(given[i-1])
-        s2 = int(given[i-2]) + int(given[i-1]) + int(given[i])
+        s1 = given[i-3] + given[i-2] + given[i-1]
+        s2 = given[i-2] + given[i-1] + given[i]
         if s1 < s2:
             ans+=1
     print(ans)
