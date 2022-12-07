d09681012b51e1582c75956299392716e9c81411
Andrew Siemen <andrew@andrewsiemen.com>
2022-12-06 19:49:06 -0500

    2019 day 2... intcode computer, yikes

--- AOC2019/day2.py
+++ AOC2019/day2.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python3
 
-input_file = 'inputs/day$'
+# WARNING - this program requires the 'match' keyword, which
+# requires python>3.10.
+
+input_file = 'inputs/day2'
 
 def process(given):
     with open(given) as f:
@@ -8,11 +11,55 @@ def process(given):
     infile = [x.strip() for x in infile]
     return infile
 
+def compute(given, noun, verb):
+    index = 0
+    sanitized = given[0].split(",")
+    sanitized = [int(x) for x in sanitized]
+
+    # initialize memory
+    sanitized[1] = noun
+    sanitized[2] = verb
+
+    while True:
+        match int(sanitized[index]):
+            case 99:
+                break
+            case 1:
+                pos1 = sanitized[index+1]
+                pos2 = sanitized[index+2]
+                pos3 = sanitized[index+3]
+                sanitized[pos3] = sanitized[pos1] + sanitized[pos2]
+                index += 4
+                continue
+            case 2:
+                pos1 = sanitized[index+1]
+                pos2 = sanitized[index+2]
+                pos3 = sanitized[index+3]
+                sanitized[pos3] = sanitized[pos1] * sanitized[pos2]
+                index += 4
+                continue
+            case _:
+                print("Did not find matching opcode, bailing out")
+                break
+            
+    return sanitized[0]
+
 def p1(given):
-    print("Part 1 TBD")
+    answer = compute(given, 12, 2)
+    print("Number in position 0: ",answer)
     return
 def p2(given):
-    print("Part 2 TBD")
+    done = 0
+    for i in range(0,100):
+        if done:
+            break
+        else:
+            for j in range(0,100):
+                answer = compute(given,i,j)
+                if answer == 19690720:
+                    print("The answer to part 2 is:", 100 * i + j)
+                    done = 1
+                    break
     return
 
 def main():
