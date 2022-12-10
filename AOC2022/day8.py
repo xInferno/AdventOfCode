3264592737fa222596d562ed4c546a4d223f5286
Andrew Siemen <andrew@andrewsiemen.com>
2022-12-09 21:11:13 -0500

    remove input files

---
+++ AOC2022/day8.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+
+input_file = 'inputs/day$'
+
+def process(given):
+    with open(given) as f:
+        infile = f.readlines()
+    infile = [x.strip() for x in infile]
+    return infile
+
+def p1(given):
+    print("Part 1 TBD")
+    return
+def p2(given):
+    print("Part 2 TBD")
+    return
+
+def main():
+    given = process(input_file)       
+    p1(given)
+    p2(given)
+
+if __name__ == '__main__':
+    main()
